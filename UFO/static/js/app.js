84be9dcf07b4ce90e555de32959816602397032a
Benjamin Barr <benwbarr@gmail.com>
2020-12-17 13:18:59 -0700

    Worked on level 1 in app.js

--- UFO/static/js/app.js
+++ UFO/static/js/app.js
@@ -3,3 +3,13 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var dataPopulate = (dataInput) => {
+    dataInput.forEach(ufoInfo => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoInfo[column])
+        )
+    });
+}
+dataPopulate(data);
